529e0c621528005cce601babad1481efb87c6df0
shola <michael.o.situ@gmail.com>
2025-01-30 11:26:49 -0800

    completede deriving types from values exercises

--- src/040-deriving-types-from-values/131-naming-values-and-types-the-same.explainer.2.ts
+++ src/040-deriving-types-from-values/131-naming-values-and-types-the-same.explainer.2.ts
@@ -1,3 +1,7 @@
+/**
+ * When you have a value and a type with the same name, you can import
+ * both at the same time in typescript!
+ */
 import { Logger } from "./131-naming-values-and-types-the-same.explainer.1";
 
 const myApp = (logger: Logger) => {
